0c7730a6dc8ae0507611b936199b9806bc2fa1e9
kotaro_sakae <ksakae1216@gmail.com>
2019-11-12 20:59:56 +0900

    23th lesson finish

---
+++ src/main/java/jp/org/web/controller/IndexController.java
@@ -0,0 +1,38 @@
+package jp.org.web.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.org.web.form.LoginForm;
+import jp.org.web.message.LoginMessage;
+import jp.org.web.repository.LoginRepository;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class IndexController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Index screen display");
+		
+		return "index";
+	}
+}
